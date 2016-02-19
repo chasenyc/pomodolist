2e7dce0632054dc94a336497f7eba9e303833dfc
Alex Harris <chasenyc@gmail.com>
2016-02-19 17:20:28 -0500

    Add boilerplate session methods to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+  helper_method :current_user, :user_logged_in?
+
+  def current_user
+    User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def sign_in!(user)
+    @current_user = user
+    @current_user.reset_session_token!
+    self.session[:session_token] = @current_user.session_token
+  end
+
+  def sign_out!
+    current_user.try(:reset_session_token!)
+    self.session[:session_token] = nil
+  end
+  
 end
