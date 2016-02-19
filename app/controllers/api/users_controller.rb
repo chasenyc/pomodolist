e77f4f4d15d2910361a312030c351ccf1833e121
Alex Harris <chasenyc@gmail.com>
2016-02-19 12:47:16 -0500

    Add users controller.

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,34 @@
+class Api::UsersController < ApplicationController
+  
+  before_action :ensure_current_user_is_authorized, only: [:update]
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:session_token] = @user.session_token
+        render "show"
+      else
+        render json: @user.errors.full_messages.to_json, status: 401
+      end
+    end
+
+    def update
+      @user = User.find(params[:id])
+      @user.update(user_params)
+      if @user.save
+        render "show"
+      else
+        render json: @user.errors.full_messages.to_json, status: 401
+      end
+    end
+
+    private
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+
+    def ensure_current_user_is_authorized
+      return if (params[:id].to_i == current_user.id)
+      render json: {'response' => "Something went wrong, cannot edit another user's information."}, status: :forbidden
+    end
+end
