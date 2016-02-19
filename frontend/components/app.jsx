d7d816fe5a2e5c9a46099563e13df3ad6780b4e2
Alex Harris <chasenyc@gmail.com>
2016-02-19 13:17:28 -0500

    Adjust syntax for frontend entry point.

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+var React = require("react");
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Test</div>
+    );
+  }
+}
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -5,3 +5,9 @@ var Route = require("react-router").Route;
 var IndexRoute = require("react-router").IndexRoute;
 var IndexRedirect = require("react-router").IndexRedirect;
 var createHistory = require("history").createHistory;
+
+var App = require("./components/app");
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(<App />, document.getElementById('content'));
+});
