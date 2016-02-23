3dc14967ec9eb45cc2c53a734614f2ddfa758a05
Alex Harris <chasenyc@gmail.com>
2016-02-23 10:13:51 -0500

    Add precompile false.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module PomodoList
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.serve_static_assets = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
