5f45e8c20093d9d4e82c998c77d765540a75bf9b
Alex Harris <chasenyc@gmail.com>
2016-02-24 12:03:43 -0500

    Add basic seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+mark = User.create!(username: 'mark', password: 'reallySecure')
+
+Todo.create!(user: mark, title: 'send out challenges', pom_estimate: 8, pom_total: 7)
+Todo.create!(user: mark, title: 'look at a lot of pomodoros', pom_estimate: 6, pom_total: 4)
+Todo.create!(user: mark, title: 'select best candidate', pom_estimate: 2)
+Todo.create!(user: mark, title: 'find other people jobs', pom_estimate: 10)
+Todo.create!(user: mark, title: 'play some games', pom_estimate: 4, completed: true, pom_total: 1)
+Todo.create!(user: mark, title: 'afternoon nap', pom_estimate: 1, completed: true, pom_total: 2)
